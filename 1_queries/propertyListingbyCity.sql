5a0fd9d2a96e154ddc35a8670a3773f890619933
Senayson <senaialema@gmail.com>
2021-06-24 03:34:19 +0000

    Created query for most visited cities

--- 1_queries/propertyListingbyCity.sql
+++ 1_queries/propertyListingbyCity.sql
@@ -1,7 +1,7 @@
 SELECT (properties.*), avg(rating) AS average_rating
 FROM properties
 JOIN property_reviews ON property_id = properties.id
-WHERE city = 'Vancouver'
+WHERE city LIKE '%ancouv%'
 GROUP BY properties.id
 HAVING avg(rating) >= 4
 ORDER BY cost_per_night
